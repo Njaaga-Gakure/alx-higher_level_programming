9b5dfe7b8b8e27c2322fb127d19064a0916b5ec9
Njaaga-Gakure <njaagagakure@gmail.com>
2023-01-07 14:51:33 +0000

    Write a function that finds all multiples of 2 in a list

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-max_integer = __import__('9-max_integer').max_integer
-my_list = [1, 7, 89, -89, 100, 3, 6]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))
+divisible_by_2 = __import__('10-divisible_by_2').divisible_by_2
+my_list = [0, 1, 2, 3, 4, 5, 6]
+list_result = divisible_by_2(my_list)
+i = 0
+while i < len(list_result):
+	print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+	i += 1
