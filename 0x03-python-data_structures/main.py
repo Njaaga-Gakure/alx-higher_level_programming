9273d10696d3376e190c4e7f641baa381c293827
Njaaga-Gakure <njaagagakure@gmail.com>
2023-01-07 14:13:18 +0000

    Write a function that returns a tuple with the length of a string and its first character

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-add_tuple = __import__('7-add_tuple').add_tuple
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
+multiple_returns = __import__('8-multiple_returns').multiple_returns
+sentence = "At school, I learnt C!"
+str1 = "" 
+length, first = multiple_returns(str1)
+print("Length: {:d} - First character: {}".format(length, first))
