1a4c82f744c476a8a6ff79ebcbe5ed431721e55a
Njaaga-Gakure <njaagagakure@gmail.com>
2023-01-30 11:02:16 +0000

    Set and get a private instances of width and height attribute

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""A class Rectangle module."""
+
+
+class Rectangle:
+    """A class that defines a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Create a new instance of a rectangle class."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrive a private instance of a width attribute."""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """Set a private instance of a width attribute."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrive a private instance of a height attribute."""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """Set a private instance of a height attribute."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
