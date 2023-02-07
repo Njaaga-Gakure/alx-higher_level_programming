f093b0737314161f4a26f5d161da764b5fc238b5
Njaaga-Gakure <njaagagakure@gmail.com>
2023-02-07 17:04:43 +0000

    Replace all attributes of the Student instance

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Student class module."""
+
+
+class Student:
+    """A class that defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Contruct an instance of the class Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve dictionary representation of a Student instance."""
+        if isinstance(attrs, list) and all([type(i) == str for i in attrs]):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return (self.__dict__)
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance."""
+        for key, value in json.items():
+            setattr(self, key, value)
