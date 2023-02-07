26f879efdb3fd8347805874be4b15f2010f78e09
Njaaga-Gakure <njaagagakure@gmail.com>
2023-02-07 16:57:26 +0000

    Retrieve a dictionary representation of a Student instance

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -11,6 +11,8 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Retrieve dictionary representation of a Student instance."""
+        if isinstance(attrs, list) and all([type(i) == str for i in attrs]):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return (self.__dict__)
