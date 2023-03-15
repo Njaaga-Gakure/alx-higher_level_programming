da2569a9a0ea9efa9281643e602b5e22170ea9cd
Njaaga-Gakure <njaagagakure@gmail.com>
2023-03-15 09:52:18 +0000

    Write a script that creates the database hbtn_0d_2 and the user user_0d_2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- creates a user and database
+-- give SELECT privilege onl to this table
+-- script should not fail if the user or database exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'
