b986e479db46bef1584b76d6c20575e36ac33893
Njaaga-Gakure <njaagagakure@gmail.com>
2023-04-11 10:11:30 +0000

    Write a script that prints a message depending of the number of arguments passed

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argLength = process.argv.length;
+
+if (argLength === 2) {
+  console.log('No argument');
+} else if (argLength === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
