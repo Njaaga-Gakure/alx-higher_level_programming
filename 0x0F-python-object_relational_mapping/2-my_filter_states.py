71112c6521f749a5b7a5d011e65b69abaa207c9c
Njaaga-Gakure <njaagagakure@gmail.com>
2023-04-15 11:37:50 +0000

    List entry based on user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Select based on  module."""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    connect = MySQLdb.connect(
+                                host="localhost",
+                                port=3306,
+                                user=sys.argv[1],
+                                passwd=sys.argv[2],
+                                db=sys.argv[3],
+                                charset="utf8"
+                             )
+    cursor = connect.cursor()
+    query = "SELECT * FROM states\
+             WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4])
+    cursor.execute(query)
+    response = cursor.fetchall()
+    for row in response:
+        if row[1] == sys.argv[4]:
+            print(row)
+    cursor.close()
+    connect.close()
